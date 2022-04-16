866bd8593fd247d74a4ad38f679db0cc68716b25
Dell <samspurgeon109@gmail.com>
2022-04-16 11:19:13 +0530

    deleted files

--- Game.py
+++
@@ -1,7 +0,0 @@
-import random
-if __name__ == '__main__':
-    print("Welcome to the game\nGuess a number and enter between 0-9")
-    a = int(input())
-    b = random.randint(0, 9)
-    if a == b: print("You won")
-    else: print("You Loss")
